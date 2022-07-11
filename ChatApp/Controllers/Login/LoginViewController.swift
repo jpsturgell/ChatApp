10e736c5f66058991e11b7b2662dcd2203f75e11
Joshua Sturgell <jpsturgell@Joshuas-MBP-2.lan>
2022-07-10 22:53:32 -0500

    FirebaseAuth Setup

--- ChatApp/Controllers/Login/LoginViewController.swift
+++ ChatApp/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -127,6 +128,14 @@ class LoginViewController: UIViewController {
                   return
               }
         // Firebase Login
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to log in user with email: \(email)")
+                return
+            }
+            let user = result.user
+            print("Logged In User: \(user)")
+        })
     }
     
     func alertUserLoginError() {
